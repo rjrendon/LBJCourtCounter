459a35de5f5871d24b3b1d3909e368c76dbd74eb
My name <me@example.com>
2023-08-09 00:36:30 +0800

    Updated design. Added Button to negate a score by 1. Added confirmation dialog if the reset button is clicked.

--- app/src/main/java/com/android/lbjcourtcounter/MainActivity.java
+++ app/src/main/java/com/android/lbjcourtcounter/MainActivity.java
@@ -1,5 +1,9 @@
 package com.android.lbjcourtcounter;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,71 +26,112 @@ public class MainActivity extends AppCompatActivity {
      * this methods is called when 3points, 2points and 1point for team  button is clicked.
      */
 
-    public void threePointsa(View view) {
+    public void threePointsA(View view) {
         pointsA = pointsA + 3;
         displayA(pointsA);
     }
 
 
-    public void twoPointsa(View view) {
+    public void twoPointsA(View view) {
         pointsA = pointsA + 2;
         displayA(pointsA);
     }
 
-    public void onePointa(View view) {
+    public void onePointA(View view) {
         pointsA = pointsA + 1;
         displayA(pointsA);
     }
 
+    public void minOnePointA(View view) {
+        pointsA--;
+        if (pointsA < 0) {
+            pointsA = 0;
+        }
+        displayA(pointsA);
+
+    }
 
     /**
      * For Team B
      * this methods is called when 3points, 2points and 1point for team  button is clicked.
      */
 
-    public void threePointsb(View view) {
+    public void threePointsB(View view) {
         pointsB = pointsB + 3;
         displayB(pointsB);
     }
 
 
-    public void twoPointsb(View view) {
+    public void twoPointsB(View view) {
         pointsB = pointsB + 2;
         displayB(pointsB);
     }
 
-    public void onePointb(View view) {
+    public void onePointB(View view) {
         pointsB = pointsB + 1;
         displayB(pointsB);
     }
 
-/**
- * this method reset the score to zero.
- */
+    public void minOnePointB(View view) {
+        pointsB--;
+        if (pointsB < 0) {
+            pointsB = 0;
+        }
+        displayB(pointsB);
 
-public void resetButton(View view){
-    int resetScore= 0;
-    pointsA=resetScore*0;
-    pointsB=resetScore*0;
-    displayA(resetScore*0);
-    displayB(resetScore*0);
+    }
+
+    /**
+     * this method reset the score to zero.
+     */
 
 
-}
+    public void resetButton(View view) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure?");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                pointsA = 0;
+                pointsB = 0;
+                displayA(pointsA);
+                displayB(pointsB);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
     /**
      * this method is called For team A text view Points
      */
-    private void displayA(int pointsforA) {
-        TextView pointsForA = (TextView) findViewById(R.id.score_a);
-        pointsForA.setText("" + pointsforA);
+    private void displayA(int pointsForA) {
+        TextView textPointsForA = findViewById(R.id.score_a);
+        textPointsForA.setText("" + pointsForA);
     }
 
     /**
      * This method is called For team B text view Points
      */
-    private void displayB(int pointsforB) {
-        TextView pointsForB = (TextView) findViewById(R.id.score_b);
-        pointsForB.setText("" + pointsforB);
+    private void displayB(int pointsForB) {
+        TextView textPointsForB = findViewById(R.id.score_b);
+        textPointsForB.setText("" + pointsForB);
     }
 
 }
